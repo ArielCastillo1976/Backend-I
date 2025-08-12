622407bdf35842972f4e25257f1d50e4ac384e46
ArielCastillo <acastil66@gmail.com>
2025-08-12 18:59:05 -0300

    Agregada funcionalidades

---
+++ 02-BACKEND AVANZADO/02-es8.js
@@ -0,0 +1,10 @@
+const user = {
+    firstname: 'Carlos',
+    lastname: 'Rodriguez',
+    email: 'carlitos.r@mail.com',
+    age: 45
+}
+
+// console.log(Object.entries(user));
+// console.log(Object.keys(user));
+console.log(Object.values(user));
